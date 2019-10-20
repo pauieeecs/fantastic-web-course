f080e2b10d2b0634dddcc0072fac078047c8375a
Bircan Kut <48135366+bircankut@users.noreply.github.com>
2019-10-20 18:02:23 +0300

    Fix animation issue

--- Week 4/Spotify Landing Page/script.js
+++ Week 4/Spotify Landing Page/script.js
@@ -1,7 +1,14 @@
 console.log('bağlandım');
 
-function yanarliDonerliCalis() {
-    let header = document.getElementById('primary-header');
-    header.classList.toggle('yanarli');
+function animateHeader() {
+    let header1 = document.getElementById('primary-header');
+    let header2 = document.getElementById('secondary-header');
+    let button = document.getElementById('submit-button');
+    setTimeout(function () {
+        header1.innerHTML="Mutluluk :)";
+        header2.innerHTML="";
+        button.innerHTML="Teşekkürler.";
+        button.classList.remove("btn-animation");
+    },4000);
 }
 
